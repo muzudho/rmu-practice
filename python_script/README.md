9d5230e871261162e7e600be7da214b7e3e44a54
むずでょ <muzudho1@gmail.com>
2023-06-14 23:49:09 +0900

    by directory

--- python_script/README.md
+++ python_script/README.md
@@ -1,10 +1,17 @@
 # Python script
 
+👇 １ファイルずつ:  
+
 ```shell
 cd python_script/make_json_schema
 
-python by_file.py --read "C:\Users\むずでょ\Documents\GitHub\rpg-maker-unite-practice\data\json\Assets RPGMaker Storage Addon Resources addonInfos.json" --write "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json-schema/Assets RPGMaker Storage Addon Resources addonInfos.md"
+python by_file.py --read "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json/Assets RPGMaker Storage Addon Resources addonInfos.json" --write "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json-schema/Assets RPGMaker Storage Addon Resources addonInfos.md"
 
-python by_file.py --read "C:\Users\むずでょ\Documents\GitHub\rpg-maker-unite-practice\data\json\Assets RPGMaker Storage Animation JSON animation.json" --write "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json-schema/Assets RPGMaker Storage Animation JSON animation.md"
+python by_file.py --read "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json/Assets RPGMaker Storage Animation JSON animation.json" --write "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json-schema/Assets RPGMaker Storage Animation JSON animation.md"
 ```
 
+👇 ディレクトリー毎:  
+
+```shell
+python by_directory.py --read "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json/" --write "C:/Users/むずでょ/Documents/GitHub/rpg-maker-unite-practice/data/json-schema/"
+```
---
+++ python_script/make_json_schema/by_directory.py
@@ -0,0 +1,70 @@
+import argparse
+import os
+from by_file import do_it as do_it_by_file
+
+def main():
+    #
+    # コマンドライン引数取得
+    # ===================
+    #
+    # 📖 [pythonでコマンドライン引数を使うサンプル](https://qiita.com/stkdev/items/e262dada7b68ea91aa0c)
+    #
+
+    # まずオブジェクト生成
+    parser = argparse.ArgumentParser()
+
+    # 引数定義
+    parser.add_argument("--read")
+    parser.add_argument("--write")
+
+    # コマンドライン引数解析
+    args = parser.parse_args()
+
+    # 定義したものを参照できる
+    print(f"""Arguments
+=========
+
+args.read : {args.read}
+args.write: {args.write}
+""")
+
+    directory_to_read = args.read
+    directory_to_save = args.write
+
+    #
+    # 実行
+    # ===
+    #
+    do_it(directory_to_read, directory_to_save)
+
+
+def do_it(directory_to_read, directory_to_save):
+
+    #
+    # ディレクトリー内のファイル一覧を取得
+    # ===============================
+    #
+    # 📖 [Python – List Files in a Directory](https://www.geeksforgeeks.org/python-list-files-in-a-directory/)
+    #
+
+    file_entry_in_dir = os.listdir(directory_to_read)
+
+    print(f"""Paths
+=====
+""")
+
+    for file_entry in file_entry_in_dir:
+        if file_entry.endswith(".json"):
+            file_to_read = os.path.join(directory_to_read, file_entry)
+            file_to_save = os.path.join(directory_to_save, file_entry)
+
+            print(f"""
+file_to_read: {file_to_read}
+file_to_save: {file_to_save}
+""")
+            #do_it_by_file(
+            #    file_to_read,
+            #    file_to_save)
+
+if __name__ == '__main__':
+    main()
