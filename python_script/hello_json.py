ce482d773fbc5ed88c51cb98db740f707d66f735
むずでょ <muzudho1@gmail.com>
2023-06-14 18:41:15 +0900

    parse

--- python_script/hello_json.py
+++ python_script/hello_json.py
@@ -1,40 +1,56 @@
 import argparse
 import json
 
-print("Hello, JSON!")
+def main():
 
-#
-# コマンドライン引数取得
-# ===================
-#
-# 📖 [pythonでコマンドライン引数を使うサンプル](https://qiita.com/stkdev/items/e262dada7b68ea91aa0c)
-#
+    #
+    # コマンドライン引数取得
+    # ===================
+    #
+    # 📖 [pythonでコマンドライン引数を使うサンプル](https://qiita.com/stkdev/items/e262dada7b68ea91aa0c)
+    #
 
-# まずオブジェクト生成
-parser = argparse.ArgumentParser()
+    # まずオブジェクト生成
+    parser = argparse.ArgumentParser()
 
-# 引数定義
-parser.add_argument("--file")
+    # 引数定義
+    parser.add_argument("--file")
 
-# コマンドライン引数解析
-args = parser.parse_args()
+    # コマンドライン引数解析
+    args = parser.parse_args()
 
-# 定義したものを参照できる
-print(f"arg file={args.file}")
+    # 定義したものを参照できる
+    print(f"arg file={args.file}")
 
-#
-# JSON読込
-# ========
-#
-# 📖 [【Python入門】JSON形式データの扱い方](https://qiita.com/Morio/items/7538a939cc441367070d)
-# 📖 [Pythonでファイルの読み込み、書き込み（作成・追記）](https://note.nkmk.me/python-file-io-open-with/)
-#
+    #
+    # JSON読込
+    # ========
+    #
+    # 📖 [【Python入門】JSON形式データの扱い方](https://qiita.com/Morio/items/7538a939cc441367070d)
+    # 📖 [Pythonでファイルの読み込み、書き込み（作成・追記）](https://note.nkmk.me/python-file-io-open-with/)
+    #
 
-with open(args.file) as f:
-    text = f.read()
-    print(text)
-    # 文書構造へ変換
-    #document = json.load(text)
+    with open(args.file) as f:
+        # text = f.read()
+        # print(text)
+        # 文書構造へ変換
+        document = json.load(f)
 
-    # ダンプ
-    #json_str = json.dumps(document)
+        # ダンプ
+        json_str = json.dumps(document)
+        print(f"dump={json_str}")
+
+        # 解析
+        parse_member(document)
+
+def parse_member(node):
+    for key, value in node.items():
+        print(f"key={value}")
+        print(f"type(value)={type(value)}")
+
+        if isinstance(value, dict):
+            parse_member(value)
+
+
+if __name__ == '__main__':
+    main()
