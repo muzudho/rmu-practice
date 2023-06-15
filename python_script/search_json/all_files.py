760fbde4b3ab9e6b50fd5f4c452c8a0a47ef71ee
むずでょ <muzudho1@gmail.com>
2023-06-15 19:16:09 +0900

    Search JSON

--- python_script/search_json/all_files.py
+++ python_script/search_json/all_files.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 def main():
     #
@@ -23,6 +24,50 @@ def main():
 
 args.directory : {args.directory}
 """)
+
+    directory_to_read = args.directory
+
+    print("Please wait")
+    result_list = search_member(directory_to_read)
+
+    for json_file in result_list:
+        print(f"result: {json_file}")
+
+
+def search_member(directory):
+
+    #
+    # リスト
+    # =====
+    #
+    # 📖 [How To add Elements to a List in Python](https://www.digitalocean.com/community/tutorials/python-add-to-list)
+    #
+    result_list = []
+
+    #
+    # ディレクトリー内のファイル一覧を取得
+    # ===============================
+    #
+    # 📖 [Python – List Files in a Directory](https://www.geeksforgeeks.org/python-list-files-in-a-directory/)
+    #
+
+    file_entry_in_dir = os.listdir(directory)
+    # print(f"file_entry_in_dir: {file_entry_in_dir}")
+
+    for basename in file_entry_in_dir:
+        # フルパスに変更
+        file_entry_path = os.path.join(directory,basename)
+        # print(f"file_entry_path: {file_entry_path}")
+
+        if basename.endswith(".json"):
+            result_list.append(file_entry_path)
+        elif os.path.isdir(file_entry_path):
+            result_list.extend(
+                search_member(file_entry_path)
+            )
+
+    return result_list
+
 
 if __name__ == '__main__':
     main()
