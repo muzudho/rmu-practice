4c844d709fb104789cf4efc15dfa86eea80f412d
むずでょ <muzudho1@gmail.com>
2023-06-16 19:57:05 +0900

    Append br

---
+++ python_script/doc_edit/README.md
@@ -0,0 +1,14 @@
+# Doc edit
+
+## Append br
+
+temp/input.txt ファイルの全ての行の末尾に <br/> を付けたものを temp/output.txt ファイルに書き出す。  
+temp ディレクトリーは git でコミットしないように .gitignore ファイルで指定する  
+
+既に末尾に <br> が付いている行には付けない  
+
+```shell
+cd python_script/doc_edit
+
+python append_br.py
+```
---
+++ python_script/doc_edit/append_br.py
@@ -0,0 +1,43 @@
+import re
+
+def main():
+    file_to_read = "./temp/input.txt"
+    file_to_write = "./temp/output.txt"
+
+    pattern = r"<br\s*/>\s*$"
+    lines = []
+
+    #
+    # ファイル読込
+    # ===========
+    #
+    # 📖 [［Python入門］ファイル操作の基本](https://atmarkit.itmedia.co.jp/ait/articles/1909/17/news030_2.html)
+    # 📖 [【Python】ファイル読み込み【1行、全行、リスト】](https://max999blog.com/python-file-read-readline-readlines/)
+    #
+    with open(file_to_read, 'r', encoding="utf-8-sig") as f:
+        print(f"Read text file to {file_to_read}")
+        # 改行コードは省く
+        lines = f.read().split('\n')
+
+    #
+    # 行末に<br/>を付ける
+    # =================
+    #
+    # - 行末に既に<br/>が付いている間は付けない
+    #
+    text = ""
+    for line in lines:
+        if not re.match(pattern, line):
+            text += f"{line}<br/>\n"
+
+    #
+    # ファイルを保存
+    # ============
+    #
+    with open(file_to_write, 'w', encoding='utf-8-sig') as f:
+        print(f"Write text file to {file_to_write}")
+        f.write(text)
+
+
+if __name__ == '__main__':
+    main()
